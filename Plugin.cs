0534233e549a2decb759f6c7b94a8ee9a7b0509e
Bobby Shmurner <glitched.ohdeymus@gmail.com>
2023-03-23 19:04:25 +0000

    first commit

---
+++ Plugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace JusticeForPanopticon
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class Plugin : BaseUnityPlugin {
+        void Awake() {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            StartCoroutine(DisabledInstakill(SceneManager.GetActiveScene()));
+        }
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode loadMode) {
+            StartCoroutine(DisabledInstakill(scene));
+        }
+
+        private void OnDestroy() {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        IEnumerator DisabledInstakill(Scene scene) {
+            if (scene.name != "Level P-2") yield break;
+            yield return null;
+
+            bool disabledInstakill = false;
+
+            foreach (ObjectActivator activator in Resources.FindObjectsOfTypeAll<ObjectActivator>()) {
+                if (activator.gameObject.name != "DelayedInstakill") continue;
+                activator.enabled = false;
+                disabledInstakill = true;
+            }
+
+            if (disabledInstakill) {
+                Logger.LogInfo("Disabled Panopticon Instalkill!");
+            } else {
+                Logger.LogError("Failed to disabled Panopticon Instalkill!");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Justice For Panopticon
+
+Hakita unrighteously took away our beloved Panopticon before we even got a chance to get to know it.
+
+I, for one, will not stand for such injustice. So I took matters into my own hands. With this mod, you not only allow the Flesh Panopticon to live its life to its full potential, but you also get to witness our favourite little prison grow and flourish into the big and strong boss it was always destined to be ***(while it bodies you in the process).***
